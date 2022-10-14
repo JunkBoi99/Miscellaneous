b7c85f951383541b7e99688277a8fa6090942fb3
JunkBoi99 <jun.kim727@gmail.com>
2022-10-14 20:18:12 +0900

    Test

---
+++ LMAO/index.js
@@ -0,0 +1,9 @@
+setInterval(()=>{
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a,i)=>{
+    a.src = 
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FY5B0w%2FbtrODux57oC%2FOSiBBw75y1K26kmiV7kn40%2Fimg.png"
+})
+
+},500)
